54d4b81ba7e40bd3cc763bdaa2ef0d8b563c56ed
Gabriel Kassarp <gkassarp@gmail.com>
2026-09-08 15:59:53 +0200

    adjusted auth api

--- Barsonomy.Client/src/api/api-client.ts
+++ Barsonomy.Client/src/api/api-client.ts
@@ -19,14 +19,42 @@ export const apiRoutes = {
 } as const;
 
 export const api = {
-  auth: authApi,
-  beergoals: beerTargetsApi,
-  categories: categoriesApi,
-  dashboard: dashboardApi,
+  auth: {
+    register: (...args: Parameters<typeof authApi.register>) =>
+      authApi.register(...args),
+    login: (...args: Parameters<typeof authApi.login>) =>
+      authApi.login(...args),
+    refresh: (...args: Parameters<typeof authApi.refresh>) =>
+      authApi.refresh(...args),
+  },
+  beergoals: {
+    list: (...args: Parameters<typeof beerTargetsApi.list>) =>
+      beerTargetsApi.list(...args),
+    get: (...args: Parameters<typeof beerTargetsApi.get>) =>
+      beerTargetsApi.get(...args),
+    create: (...args: Parameters<typeof beerTargetsApi.create>) =>
+      beerTargetsApi.create(...args),
+    update: (...args: Parameters<typeof beerTargetsApi.update>) =>
+      beerTargetsApi.update(...args),
+    remove: (...args: Parameters<typeof beerTargetsApi.remove>) =>
+      beerTargetsApi.remove(...args),
+  },
+  categories: {
+    list: (...args: Parameters<typeof categoriesApi.list>) =>
+      categoriesApi.list(...args),
+  },
+  dashboard: {
+    get: (...args: Parameters<typeof dashboardApi.get>) =>
+      dashboardApi.get(...args),
+  },
   expenses: {
-    list: expensesApi.list,
-    create: expensesApi.create,
-    update: expensesApi.update,
-    remove: expensesApi.remove,
+    list: (...args: Parameters<typeof expensesApi.list>) =>
+      expensesApi.list(...args),
+    create: (...args: Parameters<typeof expensesApi.create>) =>
+      expensesApi.create(...args),
+    update: (...args: Parameters<typeof expensesApi.update>) =>
+      expensesApi.update(...args),
+    remove: (...args: Parameters<typeof expensesApi.remove>) =>
+      expensesApi.remove(...args),
   },
 };
